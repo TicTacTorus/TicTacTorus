4b7aa91dd106124e55c393dea0dfd1a47f2134d8
mvelten1710 <marcovelten98@gmail.com>
2020-05-08 16:35:02 +0200

    Lobbies has now a table Added: - LobbyListItem.cs (For the lobby item in the table <temporary>) - Lobbies.razor has now a table with a color gradient test

---
+++ TicTacTorus/Source/LobbyListItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TicTacTorus.Source
+{
+	public class LobbyListItem
+	{
+		public string Name { get; private set; }
+		public int PlayerCount { get; private set; }
+		public int MaxPlayerCount { get; private set; }
+		public string Status { get; private set; }
+		public string Description { get; private set; }
+
+		public LobbyListItem(string name, int currentCount, int maxCount, string status, string desc)
+		{
+			Name = name;
+			PlayerCount = currentCount;
+			MaxPlayerCount = maxCount;
+			Status = status;
+			Description = desc;
+		}
+
+		public static IList<LobbyListItem> GetAllLobbies()
+		{
+			IList<LobbyListItem> test = new List<LobbyListItem>();
+			test.Add(new LobbyListItem("Daniel's Cube World", 8, 10, "Waiting", "Cubes"));
+			test.Add(new LobbyListItem("Tim's Sauna Landschaft", 2, 5, "Waiting", "No nerds plz"));
+			test.Add(new LobbyListItem("Jack's DnD Dungeon", 12, 15, "Waiting", "Only DnD Fans!"));
+			return test;
+		}
+	}
+}
