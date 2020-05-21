82707374945a1996bc44a0dc0a42d43943aa0c27
Sarah <smuell22>
2020-05-21 12:15:36 +0200

    Interface IplayerStats und Storage.. hinzugefügt

--- TicTacTorus/Source/Persistence/IPersistentStorage.cs
+++ TicTacTorus/Source/Persistence/IPersistentStorage.cs
@@ -1,7 +1,19 @@
-﻿namespace TicTacTorus.Source.Persistence
+﻿using System;
+using TicTacTorus.Pages;
+using TicTacTorus.Source.Ingame;
+using TicTacTorus.Source.Utility;
+
+namespace TicTacTorus.Source.Persistence
 {
     public interface IPersistentStorage
     {
-        
+        void SavePlayer(HumanPlayer savePlayer);
+        IPlayer LoadPlayer(string loadPlayer);
+        void SavePlayerStats(IPlayerStats savePlayStats);// PlayerStats implementiert?
+
+        IPlayerStats LoadPlayerStats(string loadPlayStats);
+        void SaveGame(Game game);
+        Replay LoadGame(Base64 base64);
+
     }
 }
