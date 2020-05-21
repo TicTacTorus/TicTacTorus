a3a349dc57d2e1b8f79943306bc4f001051368fd
Sarah <smuell22>
2020-05-21 10:59:34 +0200

    Datenbank erstellt

---
+++ TicTacTorus/Source/Persistence/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SQLite;
+namespace TicTacTorus.Source.Persistence
+
+{
+    public class Database
+    {
+        protected internal static void Init()
+        {
+            SQLiteConnection con = new SQLiteConnection("Data Source=DatabaseTicTacTorus.dat");
+            con.Open();
+            
+            SQLiteCommand command = new SQLiteCommand(con);
+           
+             command.CommandText = $"create table User (ID integer not null primary key autoincrement," +
+                     "Name varchar(100)not null)";
+            command.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
+
+
+
