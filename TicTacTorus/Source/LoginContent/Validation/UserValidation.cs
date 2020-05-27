dcbb19a4897bb6a072af5b386085dcdde59568c1
mvelten1710 <marcovelten98@gmail.com>
2020-05-27 14:31:40 +0200

    LEL

---
+++ TicTacTorus/Source/LoginContent/Validation/UserValidation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TicTacTorus.Source.LoginContent.Validation
+{
+	public class UserValidation
+	{
+		[Required]
+		[StringLength(128, MinimumLength = 1, ErrorMessage = "Name should be between 1-128 characters long!")]
+		public string Name { get; set; }
+		
+		[Required]
+		[StringLength(9999, MinimumLength = 8, ErrorMessage = "Password should be at least 8 characters long!")]
+		public string Password { get; set; }
+		
+		[Required]
+		[StringLength(9999, MinimumLength = 8, ErrorMessage = "Password should be at least 8 characters long!")]
+		public string PasswordConfirm { get; set; }
+	}
+}
