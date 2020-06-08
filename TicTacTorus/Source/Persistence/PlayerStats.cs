64ef859effaf74f1d9b66dae2a27306532e9711a
Sarah <smuell22>
2020-06-08 11:59:23 +0200

    PlayerStats added

---
+++ TicTacTorus/Source/Persistence/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacTorus.Source.Persistence
+{
+    public class PlayerStats: IPlayerStats
+    {
+        public int PlayedGames { get; set; }
+        public int WonGames { get; set; }
+        public int[] Chains { get; set; }
+
+
+        public PlayerStats(int playedGames, int wonGames, int[] chains)
+        {
+            PlayedGames = playedGames;
+            WonGames = wonGames;
+            Chains = chains;
+
+        }
+    
+    }
+}
