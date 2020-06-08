7ef4cbdfd7f4c014adcac7906175b90889d992fd
Sarah <smuell22>
2020-06-08 12:48:16 +0200

    PlayerStats added there

--- TicTacTorus/Source/PlayerSpecificContent/HumanPlayer.cs
+++ TicTacTorus/Source/PlayerSpecificContent/HumanPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using Microsoft.AspNetCore.SignalR.Client;
 using TicTacTorus.Source.LoginContent.Security;
+using TicTacTorus.Source.Persistence;
 using TicTacTorus.Source.PlayerSpecificContent;
 
 namespace TicTacTorus.Source
@@ -11,8 +12,9 @@ namespace TicTacTorus.Source
         public string InGameName { get; set; }
         public Color PlrColor { get; set; }
         public byte Symbol { get; set; }
-        public byte[] Hash { get; set; } // Besprechen
-        public byte[] Salt { get; set; }// Besprechen
+        public byte[] Hash { get; set; } 
+        public byte[] Salt { get; set; }
+        public PlayerStats playerStats { get; set; }//Besprechen
 
         public HumanPlayer(string id, string inGameName, Color plrColor, byte symbol)
         {
@@ -24,6 +26,15 @@ namespace TicTacTorus.Source
         public HumanPlayer(string id, string inGameName, Color plrColor, byte symbol, string pwd):
             this(id, inGameName, plrColor, symbol)
         {
+            SaltedHash s = new SaltedHash(pwd); // Besprechen
+            Hash = s.Hash;// Besprechen
+            Salt = s.Salt;// Besprechen
+        }
+      //playerstats insert
+        public HumanPlayer(string id, string inGameName, Color plrColor, byte symbol, string pwd,PlayerStats playerstats):
+            this(id, inGameName, plrColor, symbol)
+        {
+            this.playerStats = playerStats;
             SaltedHash s = new SaltedHash(pwd); // Besprechen
             Hash = s.Hash;// Besprechen
             Salt = s.Salt;// Besprechen
--- TicTacTorus/Source/PlayerSpecificContent/IPlayer.cs
+++ TicTacTorus/Source/PlayerSpecificContent/IPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 using Microsoft.AspNetCore.SignalR.Client;
+using TicTacTorus.Source.Persistence;
 
 namespace TicTacTorus.Source.PlayerSpecificContent
 {
@@ -9,8 +10,9 @@ namespace TicTacTorus.Source.PlayerSpecificContent
         public string InGameName { get; set; }
         public Color PlrColor { get; set; }
         public byte Symbol { get; set; }
+        public byte[] Salt { get; set; } 
+        public byte[] Hash { get; set; } 
+        public PlayerStats playerStats { get; set; }// Besprechen?
         
-        public byte[] Salt { get; set; } // Besprechen
-        public byte[] Hash { get; set; } //Besprechen
     }
 }
