32d7a1e065959468b663b7bc1405d6cacba5cc72
TimGuddat <tguddat@stud.hs-offenburg.de>
2020-06-13 16:37:22 +0200

    Fucking statistics, Motherfucker

--- TicTacTorus/Source/Persistence/PersistenceStorage.cs
+++ TicTacTorus/Source/Persistence/PersistenceStorage.cs
@@ -47,10 +47,10 @@ namespace TicTacTorus.Source.Persistence
              
 				//Player Statistic
 			
-				int[] ch = {0};
+				List<int> ch = new List<int>();
 				
 				
-				PlayerStats playerStats = new PlayerStats(0,0,ch);
+				PlayerStats playerStats = new PlayerStats(0,0, ch);
 				createPlayer.playerStats = playerStats;
 				PersistenceStorage.SavePlayerStat(createPlayer,playerStats);
 
@@ -389,7 +389,7 @@ namespace TicTacTorus.Source.Persistence
 		#region PlayerStat
 		public static PlayerStats GetPlayerStat(string id)
 		{
-			PlayerStats playerstat =null;
+			PlayerStats playerstat = new PlayerStats();
 			
 			var _con = new SQLiteConnection("Data Source=DatabaseTicTacTorus.dat");
 			_con.Open();
@@ -399,15 +399,17 @@ namespace TicTacTorus.Source.Persistence
 			                      $"where c.PlayerName = '"+ id +"' and c.PlayerName=p.PlayerName";
 			
 			var reader = command.ExecuteReader();
-			var pg =0;
-			var wg = 0;
-			int[] ch = null;
+			
+			int pg;
+			int wg;
+			List<int> ch = new List<int>();
 			while (reader.Read())
-			{ pg= (int) reader[1];
-				 wg = (int) reader[2];
+			{ 
+				pg= (int) reader[1];
+				wg = (int) reader[2];
+				ch.Add((int)reader[4]);
 				
-				ch[((int) reader[3]) - 1] = (int)reader[4];	
-				playerstat = new PlayerStats(pg,wg,ch);
+				playerstat = new PlayerStats(pg,wg, ch);
 			}
 			
 			_con.Close();
@@ -438,7 +440,7 @@ namespace TicTacTorus.Source.Persistence
 				
 				command2.ExecuteNonQuery();
 
-				for (var iter = 1; iter <= playStNewDif.Chains.Length; iter++)
+				for (var iter = 1; iter <= playStNewDif.Chains.Count; iter++)
 				{
 					command2.CommandText = $"INSERT INTO Chains(PlayerName, Length, Value) VALUES ('" + player.ID +
 					                      "'," + iter + "," + playStNewDif.Chains[iter - 1] + ")";
@@ -456,7 +458,7 @@ namespace TicTacTorus.Source.Persistence
 				reader = command.ExecuteReader();
 				var pg = 0;
 				var wg = 0;
-				int[] ch = null;
+				List<int> ch = new List<int>();
 				while (reader.Read())
 				{
 					pg = (int) reader[1];
@@ -475,9 +477,9 @@ namespace TicTacTorus.Source.Persistence
 
 				//unterscheiden ob playStNewDif.Chains.Length länger als exist
 				//Also ob neue Werte dazu kommen
-				if (existPlayerStats.Chains.Length < playStNewDif.Chains.Length)
+				if (existPlayerStats.Chains.Count < playStNewDif.Chains.Count)
 				{
-					for (var iter = 0; iter < existPlayerStats.Chains.Length; iter++)
+					for (var iter = 0; iter < existPlayerStats.Chains.Count; iter++)
 					{
 						var iter2 = iter + 1;
 						existPlayerStats.Chains[iter] += playStNewDif.Chains[iter];
@@ -488,7 +490,7 @@ namespace TicTacTorus.Source.Persistence
 
 					}
 
-					for (var iter = existPlayerStats.Chains.Length; iter < playStNewDif.Chains.Length; iter++)
+					for (var iter = existPlayerStats.Chains.Count; iter < playStNewDif.Chains.Count; iter++)
 					{
 						var iter2 = iter + 1;
 						command2.CommandText = $"insert into Chains (PlayerName,Length,Value) values('" + player.ID +
@@ -499,7 +501,7 @@ namespace TicTacTorus.Source.Persistence
 				}
 				else
 				{
-					for (var iter = 0; iter < playStNewDif.Chains.Length; iter++)
+					for (var iter = 0; iter < playStNewDif.Chains.Count; iter++)
 					{
 						var iter2 = iter + 1;
 						existPlayerStats.Chains[iter] += playStNewDif.Chains[iter];
